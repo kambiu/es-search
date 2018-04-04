ade5d48ad5cc9bba355e554724eedcf2cd57c26c
billwu <billwu@linkpower.com>
2018-04-04 15:33:51 +0800

    updated boostrap issue

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,37 +1,18 @@
 <template>
   <div class="search-container">
-      <div>
-        <b-alert show>Default Alert</b-alert>
-
-        <b-alert variant="success" show>Success Alert</b-alert>
-
-        <b-alert variant="danger"
-                dismissible
-                :show="showDismissibleAlert"
-                @dismissed="showDismissibleAlert=false">
-          Dismissible Alert!
-        </b-alert>
-
-        <b-alert :show="dismissCountDown"
-                dismissible
-                variant="warning"
-                @dismissed="dismissCountDown=0"
-                @dismiss-count-down="countDownChanged">
-          <p>This alert will dismiss after {{dismissCountDown}} seconds...</p>
-          <b-progress variant="warning"
-                      :max="dismissSecs"
-                      :value="dismissCountDown"
-                      height="4px">
-          </b-progress>
-        </b-alert>
-
-        <b-btn @click="showAlert" variant="info" class="m-1">
-          Show alert with count-down timer
-        </b-btn>
-        <b-btn @click="showDismissibleAlert=true" variant="info" class="m-1">
-          Show dismissible alert ({{showDismissibleAlert?'visible':'hidden'}})
-        </b-btn>
-      </div>
+    <!--<img src="../assets/logo.jpg">-->
+    <b-container>
+      <b-row align-h="center" align-v="center" class="el_form_input">
+        <b-col md="8">
+          <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+            <b-form-input id="input_text" class="el_form_input" v-model="text" 
+                          autocomplete="off" placeholder="Enter some words" size="lg" required />
+            <b-button type="submit" class="el_form_input" variant="primary" size="lg">Submit</b-button>
+            <b-button type="reset" class="el_form_input" variant="danger" size="lg">Reset</b-button>
+          </b-form>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -40,11 +21,37 @@ export default {
   name: 'Search',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      request: {
+        text: "",
+        opt: ""
+      },
+      show: true
+    }
+  },
+  methods: {
+    onSubmit (evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.text));
+    },
+    onReset (evt) {
+      evt.preventDefault();
+      /* Reset our form values */
+      this.text = ""
+      /* Trick to reset/clear native browser form validation state */
+      this.show = false;
+      this.$nextTick(() => { this.show = true });
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.el_form_input {
+  margin: 5px;
+  text-align:center;
+}
 </style>
